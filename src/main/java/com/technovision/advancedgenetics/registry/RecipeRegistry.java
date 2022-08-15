b2a5785bd8999f30d5a7d4ece32e08f7c49ccaa7
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-08-14 23:26:32 -0700

    Remove unused recipe classes

--- src/main/java/com/technovision/advancedgenetics/registry/RecipeRegistry.java
+++ src/main/java/com/technovision/advancedgenetics/registry/RecipeRegistry.java
@@ -3,8 +3,6 @@ package com.technovision.advancedgenetics.registry;
 import com.technovision.advancedgenetics.AdvancedGenetics;
 import com.technovision.advancedgenetics.common.recipe.cellanalyzer.CellAnalyzerRecipe;
 import com.technovision.advancedgenetics.common.recipe.cellanalyzer.CellAnalyzerRecipeSerializer;
-import com.technovision.advancedgenetics.common.recipe.dnaextractor.DnaExtractorRecipe;
-import com.technovision.advancedgenetics.common.recipe.dnaextractor.DnaExtractorRecipeSerializer;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -14,9 +12,5 @@ public class RecipeRegistry {
         // Cell Analyzer
         Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(AdvancedGenetics.MOD_ID, CellAnalyzerRecipeSerializer.ID), CellAnalyzerRecipeSerializer.INSTANCE);
         Registry.register(Registry.RECIPE_TYPE, new Identifier(AdvancedGenetics.MOD_ID, CellAnalyzerRecipe.Type.ID), CellAnalyzerRecipe.Type.INSTANCE);
-
-        // DNA Extractor
-        Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(AdvancedGenetics.MOD_ID, DnaExtractorRecipeSerializer.ID), DnaExtractorRecipeSerializer.INSTANCE);
-        Registry.register(Registry.RECIPE_TYPE, new Identifier(AdvancedGenetics.MOD_ID, DnaExtractorRecipe.Type.ID), DnaExtractorRecipe.Type.INSTANCE);
     }
 }
