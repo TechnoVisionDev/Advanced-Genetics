79e57e903afcf1ad7c65b27176016e8d7e80231f
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-08-15 20:26:48 -0700

    Add functionality for milky and meaty genes

--- src/main/java/com/technovision/advancedgenetics/events/RightClickEvents.java
+++ src/main/java/com/technovision/advancedgenetics/events/RightClickEvents.java
@@ -4,8 +4,13 @@ import com.technovision.advancedgenetics.AdvancedGenetics;
 import com.technovision.advancedgenetics.api.genetics.Genes;
 import com.technovision.advancedgenetics.components.AdvancedGeneticsComponents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 
@@ -30,6 +35,26 @@ public class RightClickEvents {
                 }
                 return ActionResult.SUCCESS;
             }
+        );
+
+        // Handles the "Milky" and "Meaty" genes
+        UseEntityCallback.EVENT.register(new Identifier(AdvancedGenetics.MOD_ID, "right_click_entity"),
+                (player, world, hand, entity, hitResult) -> {
+                    if (world.isClient()) return ActionResult.PASS;
+                    if (entity instanceof PlayerEntity clickedPlayer) {
+                        // Milk player
+                        ItemStack stack = player.getMainHandStack();
+                        if (stack.getItem() == Items.BUCKET && clickedPlayer.getComponent(AdvancedGeneticsComponents.PLAYER_GENETICS).containsGene(Genes.MILKY)) {
+                            player.setStackInHand(Hand.MAIN_HAND, new ItemStack(Items.MILK_BUCKET));
+                        }
+                        // Shear porkchops off player
+                        else if (stack.getItem() == Items.SHEARS && clickedPlayer.getComponent(AdvancedGeneticsComponents.PLAYER_GENETICS).containsGene(Genes.MEATY)) {
+                            clickedPlayer.dropStack(new ItemStack(Items.PORKCHOP));
+                            player.getMainHandStack().damage(1, player, (e) -> player.sendToolBreakStatus(player.getActiveHand()));
+                        }
+                    }
+                    return ActionResult.SUCCESS;
+                }
         );
     }
 }
